9fa2f0216f2790512e01e40ae3d8e356ef5c8449
Parashar Vinay <nebula00465@gmail.com>
2026-05-01 15:33:59 +0530

    fix: update App component to display current date and sum of two numbers

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 
-function App() {
-  console.log('om namah shivaya')
+fconst App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
 
   return (
     <div>
-      <p>shri ganeshaye namah</p>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
     </div>
   )
 }

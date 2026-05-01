23679a6b64cc105be01e0a2cab5604cfe5545cf5
Parashar Vinay <nebula00465@gmail.com>
2026-05-01 15:47:12 +0530

    fix: update App component to use React.createElement with new values for a and b

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,3 +18,18 @@ const App = () => {
 
 export default App
 
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p', null, 'Hello world, it is ', now.toString()
+    ),
+    React.createElement(
+      'p', null, a, ' plus ', b, ' is ', a + b
+    )
+  )
+}

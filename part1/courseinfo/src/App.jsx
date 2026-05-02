6c285e184ffe06abb102afda986e17b64c7a442d
Parashar Vinay <nebula00465@gmail.com>
2026-05-02 20:11:20 +0530

    Add initial setup for React application with Vite
    - Created main entry point for React application in main.jsx
    - Added Vite configuration file for project setup
    - Introduced global CSS styles in index.css
    - Included SVG assets for React and Vite logos

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,12 @@
 import './App.css'
 
-const Namaste = ({ name, age }) => {
-  console.log('namaste')
-  return (
-    <div>
-      <p>
-        Namaste {name}, you are {age} years old
-      </p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  
-  console.log('footer')
-return (
-    <div>
-      greeting app created by{' '}
-      <a href="https://fullstackopen.com/en/">
-        Full Stack Open
-      </a>
-    </div>
-  )
-}
 const App = () => {
+  
   const friends = [
     { name: 'Peter', age: 4 },
     { name: 'Maya', age: 10 },
   ]
-
+console.log(friends)
   return (
     <div>
       <p>{friends[0].name} {friends[0].age}</p>

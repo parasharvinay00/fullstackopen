de4c7b9aab2eef623af481585976f326f0f09770
Parashar Vinay <nebula00465@gmail.com>
2026-05-02 16:31:15 +0530

    fix: refactor App component to simplify structure and retain footer

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,14 +9,24 @@ const footer = () => {
     </div>
   )
 }
-
 const App = () => {
+  return [
+    <h1>Greetings</h1>,
+    <Hello name='Maya' age={26 + 10} />,
+    <Footer />
+  ]
+}
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
   return (
-    <div>
-      <h1>Greetings</h1>  
-    
-      <footer />
-    </div>
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
   )
 }
 export default App

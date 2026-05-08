d131f69f975df4c3dd85a35c4423982a719bbcea
Parashar Vinay <nebula00465@gmail.com>
2026-05-08 19:56:28 +0530

    refactor: improve App component structure by extracting Display and Button components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
+      <Display counter={counter} />
 
-      <button onClick={() => setCounter(0)}> 
-        zero
-      </button>
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   )
 }
+
 export default App

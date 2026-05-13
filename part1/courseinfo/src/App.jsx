ede826fe3f67da17a3fa8614b28ad99c7eb8ff69
Parashar Vinay <nebula00465@gmail.com>
2026-05-13 18:05:31 +0530

    refactor: remove unused logging and state management in App component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,32 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const App = () => {
-  console.log('App: render start')
   const [value, setValue] = useState(10)
-  console.log('App: state snapshot during render', { value })
 
-  useEffect(() => {
-    console.log('App: value state changed', { value })
-  }, [value])
+const hello = (who) => {
+  return () => {
+    console.log('hello', who)
+  }
+}
 
-  console.log('App: rendering JSX')
   return (
     <div>
-      <p>App.jsx updated</p>
       {value}
-      <button
-        onClick={() => {
-          console.log('App: reset button clicked', {
-            previousValue: value,
-            nextValue: 0,
-          })
-          setValue(0)
-        }}
-      >
-        reset to zero
-      </button>
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
     </div>
   )
 }
-
 export default App

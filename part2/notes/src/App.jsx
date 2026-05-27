c82854bcddfda9ac06862bde7775eed0151c43e1
Parashar Vinay <nebula00465@gmail.com>
2026-05-27 22:23:41 +0530

    refactor: update App and Note components to improve prop handling and logging

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,13 +1,23 @@
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  console.log('1. App received props:', props)
+
+  const { notes } = props
+
+  console.log('2. Notes extracted in App:', notes)
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
-          <Note key={note.id} note={note} />
-        )}
+        {notes.map(note => {
+          console.log('3. App is creating a Note for:', note)
+
+          return (
+            <Note key={note.id} note={note} />
+          )
+        })}
       </ul>
     </div>
   )
--- part2/notes/src/components/Note.jsx
+++ part2/notes/src/components/Note.jsx
@@ -1,4 +1,8 @@
-const Note = ({ note }) => {
+const Note = (props) => {
+  console.log('4. Note received props:', props)
+
+  const { note } = props
+
   return <li>{note.content}</li>
 }
 

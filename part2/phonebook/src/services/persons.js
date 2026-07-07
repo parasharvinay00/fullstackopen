9ff94dcf1273f2e81df21fe396ce799db339b075
Parashar Vinay <nebula00465@gmail.com>
2026-07-07 17:57:12 +0530

    refactor: enhance logging for API requests and responses in persons service

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,20 +1,39 @@
 import axios from 'axios'
 
-const baseUrl = '/api/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
-    return axios.get(baseUrl).then(response => response.data)
+    console.log('GET request to:', baseUrl)
+
+    return axios.get(baseUrl).then(response => {
+        console.log('Data received from backend:', response.data)
+        return response.data
+    })
 }
 
 const create = newObject => {
-    return axios.post(baseUrl, newObject).then(response => response.data)
+    console.log('POST request to:', baseUrl)
+    console.log('Data being sent:', newObject)
+
+    return axios.post(baseUrl, newObject).then(response => {
+        console.log('Created person:', response.data)
+        return response.data
+    })
 }
 
 const update = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}`, newObject).then(response => response.data)
+    console.log('PUT request to:', `${baseUrl}/${id}`)
+    console.log('Updated data:', newObject)
+
+    return axios.put(`${baseUrl}/${id}`, newObject).then(response => {
+        console.log('Updated person:', response.data)
+        return response.data
+    })
 }
 
 const remove = id => {
+    console.log('DELETE request to:', `${baseUrl}/${id}`)
+
     return axios.delete(`${baseUrl}/${id}`)
 }
 
